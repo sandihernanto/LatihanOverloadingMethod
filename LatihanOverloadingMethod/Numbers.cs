db45269054fdf86ce05bbc87ea8e1ea256f1b4f4
ACER <ACER@LAPTOP-FQJJHO85>
2022-04-08 15:48:34 +0700

    Add project files.

---
+++ LatihanOverloadingMethod/Numbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    internal class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            if (number1 < number2) return number1;
+            else return number2; 
+   
+          
+        }
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            int FindMinimum1=(number1<number2) ? number1 : number2;
+            return ((int)number3 < FindMinimum1) ? (int)number3 : FindMinimum1;
+            
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            if (number1 > number2) return number1;
+            else return number2;
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            int FindMaximum1 = (number1 > number2) ? number1 : number2;
+            return ((int)number3 > FindMaximum1) ? (int)number3 : FindMaximum1;
+        }
+    }
+}
---
+++ LatihanOverloadingMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers number=new Numbers();
+
+            Console.WriteLine(" Minimum #1 : {0}", number.FindMinimum(3,1));
+            Console.WriteLine(" Minimum #1 : {0}", number.FindMinimum(3,2,4 ));
+            Console.WriteLine();
+            Console.WriteLine(" Maximum #1 : {0}", number.FindMaximum(3, 1));
+            Console.WriteLine(" Maximum #1 : {0}", number.FindMaximum(3, 2,4));
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
